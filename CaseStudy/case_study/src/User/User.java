52b76d17f6cfae537abd062db34547d77c99f546
sn0509 <sn050920@.com>
2023-11-07 09:02:51 +0700

    add new file

--- CaseStudy/case_study/src/User/User.java
+++ CaseStudy/case_study/src/User/User.java
@@ -1,5 +1,30 @@
 package User;
 
-public interface User {
+import java.util.Scanner;
 
+public abstract class UserProperties {
+    Scanner SCANNER = new Scanner(System.in);
+    private String name = SCANNER.nextLine();
+    private String password= SCANNER.nextLine();
+
+    public UserProperties() {
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
