75a13103a83b2ef983a4c132f9659845ef4bba2e
sn0509 <144189061+sn0509@users.noreply.github.com>
2023-11-13 08:43:30 +0700

    Add files via upload

---
+++ lesson 22/22_2_Th_facade/Facade/src/Client.java
@@ -0,0 +1,7 @@
+public class Client {
+    public void share(String message){
+        SocialMediaFacade socialMediaFacade = new SocialMediaFacade(new Twitter(),new Facebook(),
+                new LinkedIn());
+        socialMediaFacade.share(message);
+    }
+}
